84b22e2811107bd50fd981e4795aa00f0c48746a
tete <sa.leonardo13@gmail.com>
2022-06-06 21:44:59 -0300

    update 06/06

---
+++ converter.py
@@ -0,0 +1,21 @@
+from xhtml2pdf import pisa             
+
+def convert_html_to_pdf(source_html, output_filename):
+    # open output file for writing (truncated binary)
+    result_file = open(output_filename, "w+b")
+
+    # convert HTML to PDF
+    pisa_status = pisa.CreatePDF(
+            source_html,                # the HTML to convert
+            dest=result_file)           # file handle to recieve result
+
+    # close output file
+    result_file.close()                 # close output file
+
+    # return False on success and True on errors
+    return pisa_status.err
+
+# Define your data
+source_html = open('documento2.html')
+output_filename = "test.pdf"
+convert_html_to_pdf(source_html, output_filename)
